95f227ba46de9e394da4dde17ae767c35562451a
STprojct <kiziyepi8@gmail.com>
2024-02-24 22:43:29 +0400

    add demo login

--- home/src/App.js
+++ home/src/App.js
@@ -7,20 +7,23 @@ import Sidebar from './components/Sidebar/Sidebar';
 
 import Home from './pages/Home';
 import Courses from './pages/Courses';
+import Login from './pages/Login';
+
 
 //<Route path='/courses/:id' element={<Course id=/>
 function App() {
   return (
-    <div>
+      <div>
       <Header/>
       <Sidebar/>
       <main id='main' class='main'>
-      <Routes>
-         <Route exact path='/' element={<Home/>}/>
-         <Route path='/courses/' element={<Courses/>}/>
-      </Routes>
+        <Routes>
+          <Route exact path='/' element={<Home/>}/> 
+          <Route path='/courses/' element={<Courses/>}/>
+          <Route path='/login/' element={<Login/>}/>
+        </Routes>
       </main>
-    </div>
+      </div>
   );
 }
 
